ab887bdea2303abece53b1ff888bb09afba542f7
Sebastian Gottschall <s.gottschall@dd-wrt.com>
2019-11-27 14:58:03 +0400

    add zstd compression
    Signed-off-by: Sebastian Gottschall <s.gottschall@dd-wrt.com>

--- module/zfs/dmu_recv.c
+++ module/zfs/dmu_recv.c
@@ -591,6 +591,13 @@ dmu_recv_begin_check(void *arg, dmu_tx_t *tx)
 		dsflags |= DS_HOLD_FLAG_DECRYPT;
 	}
 
+	if ((featureflags & DMU_BACKUP_FEATURE_ZSTD) &&
+	    !spa_feature_is_enabled(dp->dp_spa, SPA_FEATURE_ZSTD_COMPRESS))
+		return (SET_ERROR(ENOTSUP));
+
+	if (!(DMU_STREAM_SUPPORTED(featureflags)))
+		return (SET_ERROR(ENOTSUP));
+
 	error = dsl_dataset_hold_flags(dp, tofs, dsflags, FTAG, &ds);
 	if (error == 0) {
 		/* target fs already exists; recv into temp clone */
@@ -2251,20 +2258,22 @@ receive_read_record(dmu_recv_cookie_t *drc)
 			    !!DRR_IS_RAW_BYTESWAPPED(drrw->drr_flags) ^
 			    drc->drc_byteswap;
 
+			/* XXX: Allan: need complevel */
 			abuf = arc_loan_raw_buf(dmu_objset_spa(drc->drc_os),
 			    drrw->drr_object, byteorder, drrw->drr_salt,
 			    drrw->drr_iv, drrw->drr_mac, drrw->drr_type,
 			    drrw->drr_compressed_size, drrw->drr_logical_size,
-			    drrw->drr_compressiontype);
+			    drrw->drr_compressiontype, 0);
 		} else if (DRR_WRITE_COMPRESSED(drrw)) {
 			ASSERT3U(drrw->drr_compressed_size, >, 0);
 			ASSERT3U(drrw->drr_logical_size, >=,
 			    drrw->drr_compressed_size);
 			ASSERT(!is_meta);
+			/* XXX: Allan: need complevel */
 			abuf = arc_loan_compressed_buf(
 			    dmu_objset_spa(drc->drc_os),
 			    drrw->drr_compressed_size, drrw->drr_logical_size,
-			    drrw->drr_compressiontype);
+			    drrw->drr_compressiontype, 0);
 		} else {
 			abuf = arc_loan_buf(dmu_objset_spa(drc->drc_os),
 			    is_meta, drrw->drr_logical_size);
@@ -2335,11 +2344,12 @@ receive_read_record(dmu_recv_cookie_t *drc)
 			    !!DRR_IS_RAW_BYTESWAPPED(drrs->drr_flags) ^
 			    drc->drc_byteswap;
 
+			/* XXX: Allan: need complevel */
 			abuf = arc_loan_raw_buf(dmu_objset_spa(drc->drc_os),
 			    drrs->drr_object, byteorder, drrs->drr_salt,
 			    drrs->drr_iv, drrs->drr_mac, drrs->drr_type,
 			    drrs->drr_compressed_size, drrs->drr_length,
-			    drrs->drr_compressiontype);
+			    drrs->drr_compressiontype, 0);
 		} else {
 			abuf = arc_loan_buf(dmu_objset_spa(drc->drc_os),
 			    DMU_OT_IS_METADATA(drrs->drr_type),
