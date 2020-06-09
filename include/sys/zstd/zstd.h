919ad4a603b826ed77a3813ad1b39c628525a16a
Michael Niewöhner <foss@mniewoehner.de>
2020-06-10 00:07:49 +0200

    Add zstd support to zfs
    ZStandard is a modern, high performance, general compression algorithm
    originally developed by Yann Collet (the author of LZ4). It aims to
    provide similar or better compression levels to GZIP, but with much
    better performance. ZStandard provides a large selection of compression
    levels to allow a storage administrator to select the preferred
    performance/compression trade-off.
    
    This PR adds the new compression types: zstd and zstd-fast, which is
    basically a faster "negative" level of zstd.
    
    Available compression levels for zstd are zstd-1 through zstd-19. In
    addition, faster levels supported by zstd-fast are 1-10, 20-100 (in
    increments of 10), 500 and 1000. As zstd-fast is basically a "negative"
    level of zstd, it gets faster with every level in stark contrast with
    "normal" zstd which gets slower with every level.
    
    Rather than the approach used when gzip was added, of using a separate
    compression type for each different level, this PR added a new hidden
    property, compression_level. All of the zstd levels use the single entry
    in the zio_compress enum, since all levels use the same decompression
    function.
    
    However, in some cases, it was necessary to know what level a block was
    compressed within other parts of the code, so the compression level is
    stored on disk inline with the data, after the compression header
    (uncompressed size). The level is plumbed through arc_buf_hdr_t,
    objset_t, and zio_prop_t. There may still be additional places where
    this needs to be added.
    
    This new property is hidden from the user, and the user simply does zfs
    set compress=zstd-10 dataset and zfs_ioctl.c splits this single value
    into the two values to be stored in the dataset properties. This logic
    has been extended to Channel Programs as well.
    
    Closes: #6247
    Portions-Sponsored-By: The FreeBSD Foundation
    Co-authored-by: Allan Jude <allanjude@freebsd.org>
    Co-authored-by: Sebastian Gottschall <s.gottschall@dd-wrt.com>
    Co-authored-by: Kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
    Co-authored-by: Michael Niewöhner <foss@mniewoehner.de>
    Signed-off-by: Allan Jude <allanjude@freebsd.org>
    Signed-off-by: Sebastian Gottschall <s.gottschall@dd-wrt.com>
    Signed-off-by: Kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
    Signed-off-by: Michael Niewöhner <foss@mniewoehner.de>

---
+++ include/sys/zstd/zstd.h
@@ -0,0 +1,78 @@
+/*
+ * CDDL HEADER START
+ *
+ * The contents of this file are subject to the terms of the
+ * Common Development and Distribution License, Version 1.0 only
+ * (the "License").  You may not use this file except in compliance
+ * with the License.
+ *
+ * You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+ * or http://www.opensolaris.org/os/licensing.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL HEADER in each
+ * file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+ * If applicable, add the following below this CDDL HEADER, with the
+ * fields enclosed by brackets "[]" replaced with your own identifying
+ * information: Portions Copyright [yyyy] [name of copyright owner]
+ *
+ * CDDL HEADER END
+ */
+
+/*
+ * Copyright (c) 2016-2018, Klara Inc.
+ * Copyright (c) 2016-2018, Allan Jude
+ * Copyright (c) 2018-2020, Sebastian Gottschall
+ * Copyright (c) 2019-2020, Michael Niewöhner
+ */
+
+#ifndef	_ZFS_ZSTD_H
+#define	_ZFS_ZSTD_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+/*
+ * ZSTD block header
+ * NOTE: all fields in this header are in big endian order.
+ */
+typedef struct zfs_zstd_header {
+	/* Compressed size of data */
+	uint32_t c_len;
+
+	/*
+	 * Version and compression level
+	 * We use a union to be able to big endian encode a single 32 bit
+	 * unsigned integer, but still access the individual bitmasked
+	 * components easily.
+	 */
+	union {
+		uint32_t raw_version_level;
+		struct {
+			uint32_t version : 24;
+			uint8_t level;
+		};
+	};
+
+	char data[];
+} zfs_zstdhdr_t;
+
+/* (de)init for user space / kernel emulation */
+int zstd_init(void);
+void zstd_fini(void);
+
+size_t zstd_compress(void *s_start, void *d_start, size_t s_len, size_t d_len,
+    int level);
+int zstd_get_level(void *s_start, size_t s_len, uint8_t *level);
+int zstd_decompress_level(void *s_start, void *d_start, size_t s_len,
+    size_t d_len, uint8_t *level);
+int zstd_decompress(void *s_start, void *d_start, size_t s_len, size_t d_len,
+    int n);
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif /* _ZFS_ZSTD_H */
