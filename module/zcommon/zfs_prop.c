0c0f224716ccf7383d7ea3dad88888a7da953716
Michael Niewöhner <foss@mniewoehner.de>
2020-06-15 17:09:57 +0200

    [TOBESQUASHED] fix kmem tests
    Signed-off-by: Michael Niewöhner <foss@mniewoehner.de>

--- module/zcommon/zfs_prop.c
+++ module/zcommon/zfs_prop.c
@@ -424,6 +424,9 @@ zfs_prop_init(void)
 	    ZFS_TYPE_FILESYSTEM | ZFS_TYPE_VOLUME,
 	    "on | off | lzjb | gzip | gzip-[1-9] | zle | lz4 | "
 	    "zstd | zstd-[1-19] | "
+#if DEBUG
+	    "zstd-kmem-test[1-3] | "
+#endif /* DEBUG */
 	    "zstd-fast-[1-10,20,30,40,50,60,70,80,90,100,500,1000]",
 	    "COMPRESS", compress_table);
 	zprop_register_index(ZFS_PROP_SNAPDIR, "snapdir", ZFS_SNAPDIR_HIDDEN,
